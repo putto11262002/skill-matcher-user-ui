1c72a37b648fba47ab1fb0f37a9e79917563aad3
Put Suthisrisinlpa <putto11262002@gmail.com>
2023-04-05 22:46:06 +1200

    format

--- admin-client/src/components/common/TopBar.jsx
+++ admin-client/src/components/common/TopBar.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-import AppBar from "@mui/material/AppBar";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 
 const TopBar = ({ handleDrawerToggle }) => {
   return (
     <AppBar
-      position="fixed"
+      position='fixed'
       sx={{
         // width: { sm: `calc(100%)` },
         // ml: { sm: `${drawerWidth}px` },
@@ -17,15 +17,15 @@ const TopBar = ({ handleDrawerToggle }) => {
     >
       <Toolbar>
         <IconButton
-          color="inherit"
-          aria-label="open drawer"
-          edge="start"
+          color='inherit'
+          aria-label='open drawer'
+          edge='start'
           onClick={handleDrawerToggle}
-          sx={{ mr: 2, display: { sm: "none" } }}
+          sx={{ mr: 2, display: { sm: 'none' } }}
         >
           <MenuIcon />
         </IconButton>
-        <Typography variant="h6" noWrap component="div">
+        <Typography variant='h6' noWrap component='div'>
           Skill Matcher Admin
         </Typography>
       </Toolbar>
