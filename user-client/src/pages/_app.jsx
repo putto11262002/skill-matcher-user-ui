2bca3a4f12483e4e209ea0c5e093aed9d4818aa0
Put Suthisrisinlpa <putto11262002@gmail.com>
2023-04-06 10:35:07 +1200

    add global providers

--- user-client/src/pages/_app.jsx
+++ user-client/src/pages/_app.jsx
@@ -1,8 +1,20 @@
 import Layout from '@/components/common/Layout'
+import store from '@/redux/stores'
 import '@/styles/global.css'
-
+import { Provider } from 'react-redux'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { CssBaseline } from '@mui/material'
+const queryClient = new QueryClient()
 export default function App({ Component, pageProps }) {
-  return <Layout>
-    <Component {...pageProps} />
-  </Layout>
+  const getLayout = Component.getLayout || ((page) => <Layout>{page}</Layout>)
+  return <Provider store={store}>
+    <QueryClientProvider client={queryClient}>
+      <CssBaseline/>
+      {
+        getLayout(
+          <Component {...pageProps} />
+        )
+      }
+    </QueryClientProvider>
+  </Provider>
 }
