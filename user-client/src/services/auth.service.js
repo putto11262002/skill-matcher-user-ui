0c70569a267840e578f8143a1b658f673e7cc1a4
Put Suthisrisinlpa <putto11262002@gmail.com>
2023-04-06 10:03:55 +1200

    add auth related services

---
+++ user-client/src/services/auth.service.js
@@ -0,0 +1,43 @@
+import api from './api';
+import tokenService from './token.service';
+import userService from './user.service';
+
+export class AuthService {
+  async refresh() {
+    return api.post('/auth/refresh', { refreshToken: tokenService.getLocalRefreshToken() });
+  }
+
+  setLocalUser(user) {
+    if (typeof window === 'undefined') return null;
+    return window.localStorage.setItem('user', user ? JSON.stringify(user) : null);
+  }
+
+  getLocalUser() {
+    if (typeof window === 'undefined') return null;
+    return JSON.parse(window.localStorage.getItem('user'));
+  }
+
+  removeLocalUser() {
+    if (typeof window === undefined) return null;
+    return window.localStorage.removeItem('user');
+  }
+
+  async signIn(usernameOrEmail, password) {
+    const res = await api.post('/auth/sign-in', { usernameOrEmail, password });
+    const { accessToken, refreshToken, user } = await res.data;
+    tokenService.setLocalAccessToken(accessToken);
+    tokenService.setLocalRefreshToken(refreshToken);
+    this.setLocalUser(user);
+    return res;
+  }
+
+  async signOut() {
+    const res = await api.delete('/auth/sign-out');
+    tokenService.removeLocalAccessToken();
+    tokenService.removeLocalRefreshToken();
+    this.removeLocalUser();
+    return res;
+  }
+}
+
+export default new AuthService();
