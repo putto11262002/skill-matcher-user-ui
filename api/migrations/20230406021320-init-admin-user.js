6119e05876bc7e7102ec97f1d45252908b42e6ff
Put Suthisrisinlpa <putto11262002@gmail.com>
2023-04-06 14:38:32 +1200

    add migration script

---
+++ api/migrations/20230406021320-init-admin-user.js
@@ -0,0 +1,20 @@
+const bcrypt = require("bcrypt")
+module.exports = {
+  async up(db, client) {
+    // TODO write your migration here.
+    // See https://github.com/seppevs/migrate-mongo/#creating-a-new-migration-script
+    // Example:
+    // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: true}});
+    const salt = await bcrypt.genSalt();
+    const hashed = await bcrypt.hash(process.env.ROOT_USER_PASSWORD, salt)
+    await db.collection("users").insertOne({username: process.env.ROOT_USER_USERNAME, password: hashed, role: "admin", status: "active", createdAt: Date.UTC, updatedAt: Date.UTC})
+  },
+
+  async down(db, client) {
+    // TODO write the statements to rollback your migration (if possible)
+    // Example:
+    // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: false}});
+    console.log(`Deleting ${process.env.ROOT_USER_USERNAME}`)
+    await db.collection("users").deleteOne({username: process.env.ROOT_USER_USERNAME})
+  }
+};
