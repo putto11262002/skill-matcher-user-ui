d511d926241b79e2662e7ad4484bf659928bab42
Put Suthisrisinlpa <putto11262002@gmail.com>
2023-04-14 00:07:10 +1200

    feat: add in memory test mongo

--- api/src/common/mongo/mongo.module.ts
+++ api/src/common/mongo/mongo.module.ts
@@ -1,8 +1,20 @@
-import { Global, Module } from "@nestjs/common";
+import { Global, Inject, Module } from "@nestjs/common";
 import { MongoService } from "./mongo.service";
+import { MongooseModule } from "@nestjs/mongoose";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { MongoMemoryServer } from "mongodb-memory-server";
+import { MongoMemoryProvider } from "./providers/memory-mongo.provider";
+import { MongoProvider } from "./providers/mongo.provider";
 
 @Global()
 @Module({
-    providers: [MongoService]
+    imports: [
+        MongooseModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useClass: process.env.NODE_ENV === 'test' ? MongoMemoryProvider : MongoProvider
+          })
+    ],
+    providers: [MongoService, MongoMemoryProvider, MongoProvider]
 })
 export class MongoModule{}
