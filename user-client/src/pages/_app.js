f1e71f1b5c5e9a84e7821d44adcb26418ddc273f
Christine Anesone <anech264@student.otago.ac.nz>
2023-04-14 17:05:45 +1200

    feat: implement sign up page, got error with react query client

--- user-client/src/pages/_app.js
+++ user-client/src/pages/_app.js
@@ -3,9 +3,8 @@ import store from '@/redux/stores'
 import '@/styles/global.css'
 import { Provider } from 'react-redux'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { CssBaseline, ThemeProvider } from '@mui/material'
-import { theme } from '@/styles/theme'
-const queryClient = new QueryClient()
+import { CssBaseline } from '@mui/material'
+const queryClient = new QueryClient({})
 export default function App({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => <Layout>{page}</Layout>)
   return <Provider store={store}>
