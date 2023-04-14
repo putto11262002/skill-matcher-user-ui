224483c7496c8dec051db87704d49261fd3b07a4
Put Suthisrisinlpa <putto11262002@gmail.com>
2023-04-14 17:08:28 +1200

    add mui theme provider

--- user-client/src/pages/_app.js
+++ user-client/src/pages/_app.js
@@ -3,18 +3,21 @@ import store from '@/redux/stores'
 import '@/styles/global.css'
 import { Provider } from 'react-redux'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { CssBaseline } from '@mui/material'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { theme } from '@/styles/theme'
 const queryClient = new QueryClient()
 export default function App({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => <Layout>{page}</Layout>)
   return <Provider store={store}>
     <QueryClientProvider client={queryClient}>
-      <CssBaseline/>
+    <ThemeProvider theme={theme}>
+    <CssBaseline />
       {
         getLayout(
           <Component {...pageProps} />
         )
       }
+    </ThemeProvider>
     </QueryClientProvider>
   </Provider>
 }
---
+++ user-client/src/styles/theme.js
@@ -0,0 +1,4 @@
+import { createTheme } from "@mui/material";
+import { blue, grey, red } from "@mui/material/colors";
+
+export const theme = createTheme({})
