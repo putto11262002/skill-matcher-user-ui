5388fcb5f98a1595455569f430bfaf77d0724586
Put Suthisrisinlpa <putto11262002@gmail.com>
2023-04-14 18:06:36 +1200

    resolve merge confict

--- user-client/src/pages/_app.js
+++ user-client/src/pages/_app.js
@@ -3,14 +3,9 @@ import store from '@/redux/stores'
 import '@/styles/global.css'
 import { Provider } from 'react-redux'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-<<<<<<< HEAD
-import { CssBaseline } from '@mui/material'
-const queryClient = new QueryClient({})
-=======
 import { CssBaseline, ThemeProvider } from '@mui/material'
 import { theme } from '@/styles/theme'
 const queryClient = new QueryClient()
->>>>>>> 769c87dcd5754dcb3af95d55277b3836fb7e8dec
 export default function App({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => <Layout>{page}</Layout>)
   return <Provider store={store}>
