8169a9f67424a33a68f4b920df48b56058080219
Put Suthisrisinlpa <putto11262002@gmail.com>
2023-04-18 17:14:55 +1200

    add services

--- user-client/src/services/user.service.ts
+++ user-client/src/services/user.service.ts
@@ -25,7 +25,26 @@ export class UserService {
 
     // not implemented yet
     async updateAvatar(file){
-        return api.post("/user/avatar")
+        const formData = new FormData()
+        formData.append('avatar', file)
+        return api.putForm("/user/self/avatar",formData )
+    }
+
+    async addSkill(payload){
+        return api.post('/user/self/skill', payload)
+    }
+
+    async deleteSkill(skill){
+        return api.delete(`/user/self/skill/${skill}`)
+    }
+
+    async getSelfSkill(){
+        return api.get('/user/self/skill')
+    }
+
+    // get other user's skill
+    async getUserSkill(id){
+        return api.get(`/user/${id}/skill`)
     }
 }
 
