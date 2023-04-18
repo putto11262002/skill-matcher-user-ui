08da3d55f9a6e8bffa9474bed98f22a4386f9d59
Christine Anesone <anech264@student.otago.ac.nz>
2023-04-19 01:25:20 +1200

    navbar options depending on whether user is signed in/not

--- user-client/src/pages/index.jsx
+++ user-client/src/pages/index.jsx
@@ -1,10 +1,12 @@
-
 import { Inter } from 'next/font/google'
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { useSelector } from 'react-redux';
 import Head from 'next/head'
-import { Typography } from '@mui/material';
+import { 
+  Typography,
+  Link, 
+} from '@mui/material';
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -12,21 +14,31 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   const router = useRouter();
   const { isLoggedIn } = useSelector((state) => state.auth);
+
   useEffect(() => {
     if (!isLoggedIn) router.push('/login');
   }, []);
+
   return (
     <>
+      <Head>
+        <title>Skill matcher</title>
+      </Head>
 
-    <Head>
-      <title>Skill matcher</title>
-    </Head>
-   <Typography variant='1' component='h1'>H1</Typography>
+      <nav>
+        {isLoggedIn ? (
+          <Link href="/user-homepage">Dashboard</Link>
+        ) : (
+          <>
+            <Link href="/login">Log in</Link>
+            <Link href="/sign-up">Sign up</Link>
+          </>
+        )}
+      </nav>
 
-   <Typography >H1</Typography>
-
-   <Typography  sx={{display: {xs: 'none', md: 'block'}}} variant='6' component='h6'>H6</Typography>
-
+      <Typography variant='1' component='h1'>H1</Typography>
+      <Typography >H1</Typography>
+      <Typography sx={{display: {xs: 'none', md: 'block'}}} variant='6' component='h6'>H6</Typography>
     </>
   );
 }
