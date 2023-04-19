86276a12a5e7c03f11abbc959af895bdfb1dc21d
Put Suthisrisinlpa <putsuthisrisinlpa@Puts-MacBook-Air.local>
2023-04-19 20:48:48 +1200

    comment out unimplemented routes

--- api/src/modules/user/controllers/user-privacy-setting.controller.ts
+++ api/src/modules/user/controllers/user-privacy-setting.controller.ts
@@ -4,11 +4,11 @@ import { ApiTags } from '@nestjs/swagger';
 @ApiTags('User')
 @Controller('user/privacy-setting/self')
 export class UserPrivacySettingController {
-  @Put()
-  @HttpCode(HttpStatus.NO_CONTENT)
-  async updateSelfPrivacySetting() {}
+  // @Put()
+  // @HttpCode(HttpStatus.NO_CONTENT)
+  // async updateSelfPrivacySetting() {}
 
-  @Get()
-  @HttpCode(HttpStatus.OK)
-  async getSelfPrivacySetting() {}
+  // @Get()
+  // @HttpCode(HttpStatus.OK)
+  // async getSelfPrivacySetting() {}
 }
