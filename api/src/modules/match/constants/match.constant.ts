f799563020d58d1be6fcb4ce80602f2758093af2
Put Suthisrisinlpa <sutpu703@student.otago.ac.nz>
2023-04-20 19:29:45 +1200

    modify update match user status logic

--- api/src/modules/match/constants/match.constant.ts
+++ api/src/modules/match/constants/match.constant.ts
@@ -6,8 +6,10 @@ export const MATCH_STATUS = {
 
 export const MATCH_USER_STATUS = {
     ACCEPTED: 'accepted',
-    PENDING: 'pening',
+    PENDING: 'pending',
     DECLINED: 'declined'
 }
 
 export const NOT_ALLOW_UPDATE_FIELDS = ['users']
+
+export const NOT_ALLOW_USER_SEARCH_FIELDS = []
--- api/src/modules/match/services/match.service.ts
+++ api/src/modules/match/services/match.service.ts
@@ -11,9 +11,9 @@ import { FilterQuery, Model } from 'mongoose';
 import { UserService } from '../../user/services/user.service';
 import { User } from '../../user/schemas/user.schema';
 import { MATCH_STATUS, MATCH_USER_STATUS } from '../constants/match.constant';
-import { ObjectId } from 'mongodb';
+import { ObjectId } from 'mongoose';
 import { omit } from 'lodash';
-import { MatchSearchQueryDto } from '../dto/requests/match-search-query.dto';
+import { SearchMatchQueryDto } from '../dto/requests/search-match-query.dto';
 
 @Injectable()
 export class MatchService {
@@ -29,7 +29,6 @@ export class MatchService {
     user2: User,
     user2Status: string,
   ) {
-    
     const existingMatch = await this.matchModel.findOne({
       $and: [
         {
@@ -50,33 +49,35 @@ export class MatchService {
     });
 
     let match: MatchDocument;
-    if(existingMatch){
+    if (existingMatch) {
       match = existingMatch;
+
+      // if the match is already exist do not continue
+      if (existingMatch.status === MATCH_STATUS.ACTIVE) {
+        throw new BadRequestException('Match already exist');
+      }
+    } else {
+      match = await this.matchModel.create({
+        users: [{ userId: user1._id }, { userId: user2._id }],
+      });
     }
-    match = new this.matchModel({
-      users: [
-        {userId: user1._id},
-        {}
-      ]
-    })
 
-    
-
-    const createdMatch = await this.matchModel.create({
-      users: [
-        { userId: user1._id, status: user1Status },
-        { userId: user2._id, status: user2Status },
-      ],
-      status:
-        user1Status === MATCH_USER_STATUS.ACCEPTED &&
-        user2Status === MATCH_USER_STATUS.ACCEPTED
-          ? MATCH_STATUS.ACTIVE
-          : MATCH_STATUS.PENDING,
+    // update statuses
+    match.users.forEach((user) => {
+      if (user.userId === user1._id) user.status = user1Status;
+      if (user.userId === user2._id) user.status = user2Status;
     });
+
+    (match.status =
+      user1Status === MATCH_USER_STATUS.ACCEPTED &&
+      user2Status === MATCH_USER_STATUS.ACCEPTED
+        ? MATCH_STATUS.ACTIVE
+        : MATCH_STATUS.PENDING),
+      await this.matchModel.updateOne({ _id: match._id }, match);
 
     // may have to add some notification email? - new match from ....
 
-    return createdMatch;
+    return match;
   }
 
   async getMatchById(id: ObjectId) {
@@ -136,32 +137,52 @@ export class MatchService {
   }
 
   async updateMatchUserStatus(id: ObjectId, userId: ObjectId, status: string) {
-    const _id = await this.matchModel.exists({ _id: id });
-    if (!_id) {
-      throw new NotFoundException('Math does not exist');
+    const match = await this.matchModel.findOne({ _id: id });
+    if (!match) {
+      throw new NotFoundException('Match does not exist');
     }
+    if (match.status === MATCH_STATUS.ACTIVE) {
+      throw new BadRequestException('Cannot modify active match');
+    }
+
+    if (match.status === MATCH_STATUS.INACTIVE) {
+      throw new BadRequestException('Cannot modify inactive match');
+    }
+
+    let updatedMatchStatus = match.status;
+
+    // if the user have already excepted they update their status
+    const matchUser = match.users.find(user => user.userId === userId);
+    if(matchUser.status === MATCH_USER_STATUS.ACCEPTED) return match;
+
+    // if one of the user decline the match become inactive
+    if (status === MATCH_USER_STATUS.DECLINED) {
+      updatedMatchStatus = MATCH_STATUS.INACTIVE;
+    }
+
+    if (status === MATCH_USER_STATUS.ACCEPTED) {
+      // if both users have accepted the set match status to active
+      const bothAccept = match.users.every((user) => {
+        if (user.userId === userId) {
+          return status === MATCH_USER_STATUS.ACCEPTED;
+        } else {
+          return user.status === MATCH_USER_STATUS.ACCEPTED;
+        }
+      });
+
+      if (bothAccept) updatedMatchStatus = MATCH_STATUS.ACTIVE;
+    }
+
     const updatedMatch = await this.matchModel.findOneAndUpdate(
-      { _id, 'users.userId': userId },
-      { $set: { 'users.$.status': status } },
+      { _id: match._id, 'users.userId': userId },
+      { $set: { 'users.$.status': status }, status: updatedMatchStatus },
+      { new: true },
     );
 
-    // if both users have accepted the set match status to active
-    const matchActive = updatedMatch.users.every(user => user.status === MATCH_USER_STATUS.ACCEPTED);
-    if(matchActive){
-      updatedMatch.status = MATCH_STATUS.ACTIVE;
-      await updatedMatch.save()
-    }
-
-    // if one user decline set match status to inactive
-    const aUserDeclined = updatedMatch.users.some(user => user.status === MATCH_USER_STATUS.DECLINED);
-    if(aUserDeclined){
-      updatedMatch.status = MATCH_STATUS.INACTIVE;
-      await updatedMatch.save()
-    }
     return updatedMatch;
   }
 
-  async searchMatchByUser(userId: ObjectId, query: MatchSearchQueryDto) {
+  async searchMatchByUser(userId: ObjectId, query: SearchMatchQueryDto) {
     const filter: FilterQuery<Match> = {};
 
     const { users } = await this.userService.search({
@@ -223,7 +244,12 @@ export class MatchService {
       this.matchModel.countDocuments(filter),
     ]);
 
-    return { matches, total };
+    return {
+      matches,
+      total,
+      pageNumber: query.pageNumber,
+      pageSize: query.pageSize,
+    };
   }
 
   async matchExists(userId1: ObjectId, userId2: ObjectId): Promise<boolean> {
