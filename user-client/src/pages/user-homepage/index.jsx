1b6258fc268d566199fd16225063354a66d71c12
Christine Anesone <anech264@student.otago.ac.nz>
2023-04-20 16:22:08 +1200

    formating edits

--- user-client/src/pages/user-homepage/index.jsx
+++ user-client/src/pages/user-homepage/index.jsx
@@ -56,28 +56,24 @@ export default function BasicTabs() {
 
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-      <Avatar src="/images/1.png" sx={{ width: 130, height: 130, mt: 4  }} />
-      {/*<Typography variant="h6" mt={2}>Name: {user.firstName} {user.lastName}</Typography>
+      <Avatar src="/images/1.png" sx={{ width: 130, height: 130, mt: 4 }} />
+      {/*<Typography variant="h6" mt={2}>Name: {user.firstName}</Typography>
       <Typography variant="h6">Email: {user.email}</Typography>
       <Typography variant="h6">Username: {user.username}</Typography>*/}
-      <Container sx={{width: '100%', mt: 4 }}>
-        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-        <Typography variant="h5">Welcome [user]</Typography>
-        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-          <Tab label="Learning" {...a11yProps(0)} />
-          <Tab label="Tutoring" {...a11yProps(1)} />
-        </Tabs>
-        <TabPanel value={value} index={0} handleChange={handleChange}>
-          Learning Content - fetch user's skill data 
-          
-          
-        </TabPanel>
-        <TabPanel value={value} index={1} handleChange={handleChange}>
-          Tutoring Content  - fetch user's tutor data 
-        </TabPanel>
+      <Container sx={{ width: '100%', mt: 4 }}>
+        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <Tabs value={value} onChange={handleChange} aria-label="basic tabs">
+            <Tab label="Learning" {...a11yProps(0)} />
+            <Tab label="Tutoring" {...a11yProps(1)} />
+          </Tabs>
+          <TabPanel value={value} index={0} handleChange={handleChange}>
+            Learning Content - fetch user's skill data
+          </TabPanel>
+          <TabPanel value={value} index={1} handleChange={handleChange}>
+            Tutoring Content  - fetch user's tutor data
+          </TabPanel>
         </Box>
-
-      </Container>
-    </Box>
+      </Container >
+    </Box >
   );
 }
