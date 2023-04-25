45fd9db889dbe3f4ef8917dbd99dae95119f3079
Put Suthisrisinlpa <sutpu703@student.otago.ac.nz>
2023-04-25 22:43:53 +1200

    prevent infinite loading

--- user-client/src/providers/AuthProvider.jsx
+++ user-client/src/providers/AuthProvider.jsx
@@ -14,7 +14,7 @@ const AuthProvider = ({children}) => {
     if (requiredAuth && !isLoggedIn) router.push("/landing");
   }, [])
 
-  if(typeof window !== undefined && requiredAuth ){
+  if(requiredAuth && !isLoggedIn ){
     return <Box sx={{width: '100vw', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center'}}><Loader/></Box>
   }
   
