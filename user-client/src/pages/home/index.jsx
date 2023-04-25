68933f7b45ada5550ae3a2e9f300e9327bdcfcb0
Put Suthisrisinlpa <sutpu703@student.otago.ac.nz>
2023-04-25 18:03:59 +1200

    h1 too big for mobile

--- user-client/src/pages/home/index.jsx
+++ user-client/src/pages/home/index.jsx
@@ -8,9 +8,9 @@ const HomePage = () => {
         <title>Home Page | Skills Matcher</title>
         <meta name="description" content="Welcome to Skills Matcher" />
       </Head>
-      <Container maxWidth="md">
+     
         <Box sx={{ my: 4 }}>
-          <Typography variant="h1" component="h1" align="center" gutterBottom>
+          <Typography variant="h2" component="h2" align="center" gutterBottom>
             Welcome to Skills Matcher
           </Typography>
           <Typography variant="h4" component="h2" align="center" gutterBottom>
@@ -20,7 +20,7 @@ const HomePage = () => {
           start unlocking your full potential!
           </Typography>
         </Box>
-      </Container>
+    
     </>
   );
 };
