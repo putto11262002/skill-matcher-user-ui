11c9dafcd797fab0ac00df4c8a8dd51ee1877523
Christine Anesone <anech264@student.otago.ac.nz>
2023-04-25 21:39:13 +1200

    fetching user data

--- user-client/src/pages/index.jsx
+++ user-client/src/pages/index.jsx
@@ -1,22 +1,31 @@
 
 import { Inter } from 'next/font/google'
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { useSelector } from 'react-redux';
 import Head from 'next/head'
 import dynamic from 'next/dynamic';
-import { 
+import userService from '@/services/user.service';
+import {
   Typography,
   Link,
-  Button, 
+  Button,
 } from '@mui/material';
 
 
 const inter = Inter({ subsets: ['latin'] })
 
-const Home = ()  => {
+const Home = () => {
   const router = useRouter();
   const { isLoggedIn } = useSelector((state) => state.auth);
+
+  // Use useQuery to fetch the user data
+  const { isLoading, data: user } = useQuery('userData', userService.getSelf, {
+    enabled: isLoggedIn, // Only fetch the data when the user is logged in
+  });
+
+  // Use useMutation to update the user data
+  const [updateUser] = useMutation(userService.updateSelf);
 
   useEffect(() => {
     if (isLoggedIn) router.push('/home');
@@ -24,13 +33,32 @@ const Home = ()  => {
     
   }, []);
 
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      userService.getSelf().then((userData) => {
+        setUser(userData.data); // Note the use of userData.data to get the user data from the response object
+      }).catch((error) => {
+        console.log('Error fetching user data', error);
+      });
+    }
+  }, [isLoggedIn]);
+
+
   return (
     <>
       <Head>
         <title>Skill matcher</title>
       </Head>
 
-     
+      {/* Use Button instead of Link for conditional rendering */}
+
+      <Typography variant='1' component='h1'>H1</Typography>
+      {isLoggedIn && (
+        <Typography variant='h6' component='h6'>
+          Welcome, {user.firstName} {user.lastName}!
+        </Typography>
+      )}
     </>
   );
 }
