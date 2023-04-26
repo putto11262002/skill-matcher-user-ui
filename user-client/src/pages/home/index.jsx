d5b369a7c6eb013aac70c3e07d35814a664631b1
Christine Anesone <anech264@student.otago.ac.nz>
2023-04-27 00:29:16 +1200

    added button to edit profile

--- user-client/src/pages/home/index.jsx
+++ user-client/src/pages/home/index.jsx
@@ -1,26 +1,43 @@
 import Head from 'next/head';
-import { Box, Container, Typography } from '@mui/material';
+import { Box, Container, Typography, Button, Link } from '@mui/material';
 
 const HomePage = () => {
+
   return (
     <>
       <Head>
         <title>Home Page | Skills Matcher</title>
         <meta name="description" content="Welcome to Skills Matcher" />
       </Head>
-     
-        <Box sx={{ my: 4 }}>
-          <Typography variant="h2" component="h2" align="center" gutterBottom>
-            Welcome to Skills Matcher
-          </Typography>
-          <Typography variant="h4" component="h2" align="center" gutterBottom>
-          Discover new skills and connect with like-minded individuals on our cutting-edge app. 
-          Whether you&apos;re looking to learn a new skill or share your expertise with others, 
-          our platform provides the perfect one-stop solution. Join our community today and 
+      <Box>
+        <Typography variant="h2" component="h2" align="center" gutterBottom>
+          Welcome to Skills Matcher
+        </Typography>
+        <Typography variant="h4" component="h2" align="center" gutterBottom>
+          Discover new skills and connect with like-minded individuals on our cutting-edge app.
+          Whether you&apos;re looking to learn a new skill or share your expertise with others,
+          our platform provides the perfect one-stop solution. Join our community today and
           start unlocking your full potential!
-          </Typography>
-        </Box>
-    
+        </Typography>
+        <Link href="/profile-creation" underline="none">
+          <Button variant="contained" color="primary" sx={{
+            my: 4,
+            background: `linear-gradient(to bottom right, #055fef, #58a6ff)`,
+            padding: 4,
+            borderRadius: 2,
+            boxShadow: 4,
+            textAlign: "center",
+            '&:hover': {
+              background: 'linear-gradient(45deg,#055fef, #58a6ff)',
+              boxShadow: '0px 5px 15px rgba(255, 105, 135, .5)',
+            },
+          }}>
+            Get started on your profile
+          </Button>
+          
+        </Link>
+      </Box>
+
     </>
   );
 };
