facf2c122c624a35c89020f8f571f28b2529539c
Christine Anesone <anech264@student.otago.ac.nz>
2023-04-27 01:49:58 +1200

    added feed title

--- user-client/src/pages/user/browse/index.jsx
+++ user-client/src/pages/user/browse/index.jsx
@@ -21,21 +21,28 @@ const BrowseUserPage = () => {
     })
   );
   return (
-    <Box  sx={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-   <Box maxWidth={600}  >
-   {/* <SkillSlider  skills={['computer science', 'java', 'python', 'machine learning', 'amazon web services', 'google cloud', 'go', 'rust']}/> */}
+    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <Box maxWidth={600}  >
+        {/* <SkillSlider  skills={['computer science', 'java', 'python', 'machine learning', 'amazon web services', 'google cloud', 'go', 'rust']}/> */}
 
-   
-      {/* <SearchUserSection /> */}
-    {/* <Typography variant="5" component='h5' marginBottom={2}>Suggested users</Typography> */}
-     <UserProfileGrid
-        users={data?.data?.data}
-        loading={isLoadingUsers}
-        error={error}
-      />
-     
-     </Box>
-     </Box>
+        <Typography
+          variant="2"
+          textAlign="center"
+          component="h2"
+          color={(theme) => theme.palette.primary.main}
+        >
+          Feed
+        </Typography>
+        {/* <SearchUserSection /> */}
+        {/* <Typography variant="5" component='h5' marginBottom={2}>Suggested users</Typography> */}
+        <UserProfileGrid
+          users={data?.data?.data}
+          loading={isLoadingUsers}
+          error={error}
+        />
+
+      </Box>
+    </Box>
   );
 };
 
