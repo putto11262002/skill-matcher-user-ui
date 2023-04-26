93cd027e597b5c26570a8dbbac1f3f7310eebf92
Christine Anesone <anech264@student.otago.ac.nz>
2023-04-27 00:07:37 +1200

    added snackbar wrap to app

--- user-client/src/pages/_app.js
+++ user-client/src/pages/_app.js
@@ -1,26 +1,32 @@
-import Layout from '@/components/common/Layout'
-import store from '@/redux/stores'
-import '@/styles/global.css'
-import { Provider } from 'react-redux'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { CssBaseline, ThemeProvider } from '@mui/material'
-import { theme } from '@/styles/theme'
-import AuthProvider from '@/providers/AuthProvider'
-const queryClient = new QueryClient()
+import Layout from '@/components/common/Layout';
+import store from '@/redux/stores';
+import '@/styles/global.css';
+import { Provider } from 'react-redux';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { theme } from '@/styles/theme';
+import AuthProvider from '@/providers/AuthProvider';
+import { SnackbarProvider } from 'notistack';
+const queryClient = new QueryClient();
+
 export default function App({ Component, pageProps }) {
-  const getLayout = Component.getLayout || ((page) => <Layout>{page}</Layout>)
-  return <Provider store={store}>
-    <QueryClientProvider client={queryClient}>
-    <ThemeProvider theme={theme}>
-    <CssBaseline />
-     <AuthProvider>
-     {
-        getLayout(
-          <Component {...pageProps} />
-        )
-      }
-     </AuthProvider>
-    </ThemeProvider>
-    </QueryClientProvider>
-  </Provider>
+  const getLayout = Component.getLayout || ((page) => <Layout>{page}</Layout>);
+
+  return (
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <AuthProvider>
+            <SnackbarProvider
+              anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+              autoHideDuration={3000}
+            >
+              {getLayout(<Component {...pageProps} />)}
+            </SnackbarProvider>
+          </AuthProvider>
+        </ThemeProvider>
+      </QueryClientProvider>
+    </Provider>
+  );
 }
