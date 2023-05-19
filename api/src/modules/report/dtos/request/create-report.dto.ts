59393b63ae28ab0d46bb02dc518ef47d8979a106
Put Suthisrisinlpa <sutpu703@student.otago.ac.nz>
2023-05-19 14:12:39 +1200

    OpenAPI spec #8

--- api/src/modules/report/dtos/request/create-report.dto.ts
+++ api/src/modules/report/dtos/request/create-report.dto.ts
@@ -8,18 +8,18 @@ import { REPORT_CATEGORY } from '../../constants/report.constant';
 
 export class CreateReportDto {
 
-    @ApiProperty()
+    @ApiProperty({type: String, description: 'id of target user', required: false})
     @IsOptional()
     @Type(() => mongoose.Types.ObjectId)
     @Transform(toMongoObjectId)
     target?: mongoose.Types.ObjectId;
 
-    @ApiProperty()
+    @ApiProperty({type: String, description: `One of the following category ${Object.values(REPORT_CATEGORY).join(', ')}`})
     @IsOptional()
     @IsIn(Object.values(REPORT_CATEGORY))
     category?: string;
 
-    @ApiProperty()
+    @ApiProperty({required: true})
     @IsNotEmpty()
     @IsString()
     message: string;
