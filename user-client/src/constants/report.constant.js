1dad615b1ad6730c49112da8f0e41fd5a5692cdb
Put Suthisrisinlpa <sutpu703@student.otago.ac.nz>
2023-05-22 15:45:50 +1200

    add report costant

---
+++ user-client/src/constants/report.constant.js
@@ -0,0 +1,8 @@
+export const REPORT_CATEGORY = {
+    HARASSMENT: 'harassment',
+    HATE_SPEECH: 'hate_speech',
+    INAPPROPRIATE_CONTENT: 'inappropriate_content',
+    SCAM: 'scam',
+    BUG: 'bug',
+    OTHER: 'other'
+}
