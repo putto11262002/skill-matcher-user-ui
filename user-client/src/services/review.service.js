51071429386b3375d3c9c174507ab0d248e1e756
Put Suthisrisinlpa <sutpu703@student.otago.ac.nz>
2023-05-23 13:26:53 +1200

    add get review get user id

--- user-client/src/services/review.service.js
+++ user-client/src/services/review.service.js
@@ -4,6 +4,10 @@ export class ReviewService {
     async createReview(payload){
         return api.post('/review', payload)
     }
+
+    async getRiewByUser(userId){
+        return api.get(`user/${userId}/reviews`)
+    }
 }
 
 const reviewService = new ReviewService();
