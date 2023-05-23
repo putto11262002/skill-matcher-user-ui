92ae03143379b3e912b56795589215e04db1768d
Christine Anesone <anech264@student.otago.ac.nz>
2023-05-23 13:53:39 +1200

    fixed review star sizes

--- user-client/src/components/review/ReviewDialog.jsx
+++ user-client/src/components/review/ReviewDialog.jsx
@@ -12,8 +12,11 @@ import {
 } from "@mui/material";
 import { Controller, useForm } from "react-hook-form";
 import { green } from "@mui/material/colors";
+import StarBorderOutlinedIcon from '@mui/icons-material/StarBorderOutlined';
+import StarOutlinedIcon from '@mui/icons-material/StarOutlined';
+
 const ReviewDialog = ({ open, onClose, onSubmit, target }) => {
-  const { control, handleSubmit, reset} = useForm();
+  const { control, handleSubmit, reset } = useForm();
   return (
     <Dialog fullWidth maxWidth="sm" open={open} onClose={onClose}>
       <Box
@@ -25,21 +28,24 @@ const ReviewDialog = ({ open, onClose, onSubmit, target }) => {
         })}
         component="form"
       >
-        <DialogTitle>
-          Rate {target?.profile?.firstName} {target?.profile?.lastName}&apos;s
-          performance{" "}
-        </DialogTitle>
+          <DialogTitle sx={{ fontWeight: "bold" }}>
+            Rate {target?.profile?.firstName} {target?.profile?.lastName}&apos;s Performance
+          </DialogTitle>
         <DialogContent>
-          <Stack spacing={2}>
+          <Stack spacing={1}>
             <Controller
               control={control}
               name="score"
               render={({ field: { value, onChange }, fieldState }) => (
-                <Rating
-                  value={Number(value) || 0}
-                  onChange={onChange}
-                  sx={{ fontSize: "2em" }}
-                />
+                <Box display="flex" justifyContent="center" >
+                  <Rating
+                    value={Number(value) || 0}
+                    onChange={onChange}
+                    size="large"
+                    icon={< StarOutlinedIcon style={{ width: "32px", height: "32px" }} />}
+                    emptyIcon={<StarBorderOutlinedIcon style={{ width: "32px", height: "32px" }} />}
+                  />
+                </Box>
               )}
             />
 
