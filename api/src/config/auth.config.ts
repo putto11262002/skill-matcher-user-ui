d036ce7c888c7e607fd521107540e7ebd26ee399
Put Suthisrisinlpa <sutpu703@student.otago.ac.nz>
2023-05-25 17:13:28 +1200

    add docker files

--- api/src/config/auth.config.ts
+++ api/src/config/auth.config.ts
@@ -4,11 +4,11 @@ export default registerAs('auth', () => ({
   jwt: {
     accessToken: {
       secret: process.env.ACCESS_TOKEN_JWT_SECRET || 'secret',
-      expiresIn: '5m',
+      expiresIn: '1m',
     },
     refreshToken: {
       secret: process.env.REFRESH_TOKEN_JWT_SECRET || 'secret',
-      expiresIn: '7d',
+      expiresIn: '5m',
     },
   },
   hash: { saltRounds: 10 },
